fd185cb4a1f4380e742c855a273c656ac642a37a
Sotkn <sotkn1@gmail.com>
2026-08-21 08:20:09 +0200

    swap

--- src/moon_game/world/endpoints.py
+++ src/moon_game/world/endpoints.py
@@ -2,14 +2,14 @@ from moon_game.entities import Endpoint
 from moon_game.geometry import Vec2
 
 RIDGE = Endpoint(
-    id="ridge",
-    name="Хребет",
+    id="crater",
+    name="Кратер",
     position=Vec2(300, 200),
     image_key="poi1",
 )
 CRATER = Endpoint(
-    id="crater",
-    name="Кратер",
+    id="ridge",
+    name="Хребет",
     position=Vec2(1000, 300),
     image_key="poi2",
 )
